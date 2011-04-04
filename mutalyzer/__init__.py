a4bf5e55756377f8ca64851ba34420b66d587d25
Martijn Vermaat <martijn@vermaat.name>
2011-04-04 13:24:00 +0000

    Added setuptools setup.py file.
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/branches/refactor-mutalyzer-branch@245 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -1,9 +1,4 @@
-"""
-Mutalyzer, a HGVS variant nomenclature checker.
-
-Developed at the Human Genetics department of the Leiden University
-Medical Center.
-"""
+"""HGVS variant nomenclature checker."""
 
 
 # On the event of a new release, we update the __version_info__ and __date__
@@ -12,12 +7,17 @@ Medical Center.
 # ending with a 'dev' item. Also, RELEASE is set to False (indicating that
 # the __date__ value is to be ignored).
 
-__author__ = 'Leiden University Medical Center'
+RELEASE = False
+
 __version_info__ = ('2', '0', 'beta-9', 'dev')
-__version__ = '.'.join(__version_info__)
 __date__ = '31 Jan 2011'
 
-RELEASE = False
+
+__version__ = '.'.join(__version_info__)
+__author__ = 'Leiden University Medical Center'
+__contact__ = 'humgen@lumc.nl'
+__homepage__ = 'http://mutalyzer.nl'
+
 
 NOMENCLATURE_VERSION_INFO = ('2', '0')
 NOMENCLATURE_VERSION = '.'.join(NOMENCLATURE_VERSION_INFO)
---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (2, 6):
+    raise Exception('Mutalyzer requires Python 2.6 or higher.')
+
+import mutalyzer as distmeta
+
+setup(
+    name='mutalyzer',
+    version=distmeta.__version__,
+    description=distmeta.__doc__,
+    author=distmeta.__author__,
+    author_email=distmeta.__contact__,
+    url=distmeta.__homepage__,
+    packages=find_packages(exclude=['doc', 'examples', 'tests']),
+    data_files=[('/etc/init.d', ['examples/init.d/mutalyzer-batchd'])]
+    # Todo: templates, doc, bin
+)
