0c3e89d1a3a840294abfc14664ae2cfa2b86aa55
Martijn Vermaat <martijn@vermaat.name>
2011-04-05 13:17:42 +0000

    Further changes to setup.py.
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/branches/refactor-mutalyzer-branch@248 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- setup.py
+++ setup.py
@@ -13,10 +13,18 @@ setup(
     author=distmeta.__author__,
     author_email=distmeta.__contact__,
     url=distmeta.__homepage__,
+    license='Not distributable',
+    platform=['any'],
     packages=find_packages(exclude=['doc', 'extras', 'tests']),
     include_package_data=True,
-    scripts=['bin/batch_daemon'],
+    scripts=['bin/mutalyzer-batchd'],
     zip_safe=False
-    #data_files=[('/etc/init.d', ['examples/init.d/mutalyzer-batchd'])]
-    # Todo: templates, doc, bin
 )
+
+# Things not handled by this setup.py:
+# - Copy extras/config.example to /etc/mutalyzer/config
+# - Database setup
+# - Chown /var/log/mutalyzer.log
+# - Copy extras/init.d/mutalyzer-batchd to /etc/init.d/mutalyzer-batchd
+# - Copy doc to /usr/share/doc
+# Perhaps as a postinstall script?
