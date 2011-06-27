c5b68769ff2fa175d40a9eae2b1387a37f387345
Martijn Vermaat <martijn@vermaat.name>
2011-06-27 12:36:25 +0000

    Release 2.0.beta-9
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/branches/refactor-mutalyzer-branch@288 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -18,10 +18,10 @@ import os
 #
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 
-RELEASE = False
+RELEASE = True
 
-__version_info__ = ('2', '0', 'beta-9', 'dev')
-__date__ = '31 Jan 2011'
+__version_info__ = ('2', '0', 'beta-9')
+__date__ = '27 Jun 2011'
 
 
 __version__ = '.'.join(__version_info__)
