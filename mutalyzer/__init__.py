b4baea9df5980faf11a8add96615e06a412c72d6
Martijn Vermaat <martijn@vermaat.name>
2011-11-25 14:09:35 +0000

    Open development for 2.0.beta-13
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/trunk@430 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -20,9 +20,9 @@ import os
 #
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 
-RELEASE = True
+RELEASE = False
 
-__version_info__ = ('2', '0', 'beta-12')
+__version_info__ = ('2', '0', 'beta-13', 'dev')
 __date__ = '25 Nov 2011'
 
 
