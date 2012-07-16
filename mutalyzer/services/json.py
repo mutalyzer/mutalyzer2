e985d0b4c29fc3885e52c4cbb6556f951c6ce9a6
Martijn Vermaat <martijn@vermaat.name>
2012-07-16 12:58:06 +0000

    Fix for primitive JSON result datatypes
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/branches/spyne@577 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/services/json.py
+++ mutalyzer/services/json.py
@@ -12,6 +12,10 @@ from mutalyzer.services import rpc
 
 
 # HTTP/RPC application.
+# Note that we originally provided skip_depth=2 to the JsonObject constructor
+# to get rid of some annoying wrappers around the json results. However, this
+# breaks methods returning a primitive datatype (e.g. just a string). Might
+# file a bug about this on spyne.
 application = Application([rpc.MutalyzerService], tns=mutalyzer.SOAP_NAMESPACE,
-                          in_protocol=HttpRpc(), out_protocol=JsonObject(skip_depth=2),
+                          in_protocol=HttpRpc(), out_protocol=JsonObject(),
                           name='Mutalyzer')
