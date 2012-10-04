7cac3ed85b58acef5bdd582f84f1c0136f678e43
Martijn Vermaat <martijn@vermaat.name>
2012-10-04 10:22:36 +0000

    Revert skip_depth=2 in JSON output
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/trunk@615 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/services/json.py
+++ mutalyzer/services/json.py
@@ -12,7 +12,10 @@ from mutalyzer.services import rpc
 
 
 # HTTP/RPC application.
+# Output JSON can be made more consise by specifying skip_depth=2 in the
+# JsonObject constructor, but to match the API documentation better (and hence
+# have more predictable behaviour) we leave it out.
 application = Application([rpc.MutalyzerService], tns=mutalyzer.SOAP_NAMESPACE,
                           in_protocol=HttpRpc(),
-                          out_protocol=JsonObject(skip_depth=2),
+                          out_protocol=JsonObject(),
                           name='Mutalyzer')
