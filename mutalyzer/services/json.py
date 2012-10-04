dce1e05e7c6487bd641d73cf03d9f810edbaaec0
Martijn Vermaat <martijn@vermaat.name>
2012-10-04 10:12:38 +0000

    Use skip_depth=2 for JSON output (again)
    See https://github.com/arskom/spyne/issues/159
    
    
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/trunk@613 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/services/json.py
+++ mutalyzer/services/json.py
@@ -12,10 +12,7 @@ from mutalyzer.services import rpc
 
 
 # HTTP/RPC application.
-# Note that we originally provided skip_depth=2 to the JsonObject constructor
-# to get rid of some annoying wrappers around the json results. However, this
-# breaks methods returning a primitive datatype (e.g. just a string). Might
-# file a bug about this on spyne.
 application = Application([rpc.MutalyzerService], tns=mutalyzer.SOAP_NAMESPACE,
-                          in_protocol=HttpRpc(), out_protocol=JsonObject(),
+                          in_protocol=HttpRpc(),
+                          out_protocol=JsonObject(skip_depth=2),
                           name='Mutalyzer')
