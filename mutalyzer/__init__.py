24024e0e720a790c1abd6c81ca08d7c596300e5a
Martijn Vermaat <martijn@vermaat.name>
2013-03-25 12:12:34 +0000

    Open development for 2.0.beta-26
    git-svn-id: https://humgenprojects.lumc.nl/svn/mutalyzer/trunk@675 eb6bd6ab-9ccd-42b9-aceb-e2899b4a52f1

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -20,9 +20,9 @@ import os
 #
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 
-RELEASE = True
+RELEASE = False
 
-__version_info__ = ('2', '0', 'beta-25')
+__version_info__ = ('2', '0', 'beta-26', 'dev')
 __date__ = '25 Mar 2013'
 
 
