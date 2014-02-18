5c1e01dceb3a642d732aff6e90cdf6250c571414
Martijn Vermaat <martijn@vermaat.name>
2014-02-18 10:08:50 +0100

    Open development for 2.0.beta-31

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -20,9 +20,9 @@ import os
 #
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 
-RELEASE = True
+RELEASE = False
 
-__version_info__ = ('2', '0', 'beta-30')
+__version_info__ = ('2', '0', 'beta-31', 'dev')
 __date__ = '18 Feb 2014'
 
 
