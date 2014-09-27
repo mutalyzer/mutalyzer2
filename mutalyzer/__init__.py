8fda3e51720355530b322c5298abe7aa199a4ee9
Martijn Vermaat <martijn@vermaat.name>
2014-09-27 09:49:21 +0200

    Open development for 2.0.2

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -14,7 +14,7 @@ HGVS variant nomenclature checker.
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 # [2] http://semver.org/
 
-__version_info__ = ('2', '0', '1')
+__version_info__ = ('2', '0', '2', 'dev')
 __date__ = '27 Sep 2014'
 
 
