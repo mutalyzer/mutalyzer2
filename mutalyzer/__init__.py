5c09840067327c73a9544c1a997888cc775ac6fc
Martijn Vermaat <martijn@vermaat.name>
2014-11-19 16:47:20 +0100

    Open development for 2.0.5

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -21,7 +21,7 @@ from __future__ import unicode_literals
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 # [2] http://semver.org/
 
-__version_info__ = ('2', '0', '4')
+__version_info__ = ('2', '0', '5', 'dev')
 __date__ = '19 Nov 2014'
 
 
