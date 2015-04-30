Elenali08/mutalyzer

Renamed variables and functions to pep8 style.

mutalyzer/models.py
@@ -98,7 +98,7 @@ class RawVar(ComplexModel):
     """
     __namespace__ = SOAP_NAMESPACE
 
-    DNA = Mandatory.Boolean
+    dna = Mandatory.Boolean
     start = Mandatory.Integer
     start_offset = Mandatory.Integer
     end = Mandatory.Integer
@@ -111,11 +111,11 @@ class RawVar(ComplexModel):
     deleted = Mandatory.String
     inserted = Mandatory.String
     shift = Mandatory.Integer
-    startAA = Mandatory.String
-    endAA = Mandatory.String
+    start_aa = Mandatory.String
+    end_aa = Mandatory.String
     term = Mandatory.Integer
     hgvs = Mandatory.String
-    hgvsLength = Mandatory.Integer
+    hgvs_length = Mandatory.Integer
 #RawVar
 
 

mutalyzer/mutalyzer2

mutalyzer/models.py
@@ -105,7 +105,7 @@ class RawVar(ComplexModel):
     """
     __namespace__ = SOAP_NAMESPACE
 
-    DNA = Mandatory.Boolean
+    dna = Mandatory.Boolean
     start = Mandatory.Integer
     start_offset = Mandatory.Integer
     end = Mandatory.Integer
@@ -118,11 +118,11 @@ class RawVar(ComplexModel):
     deleted = Mandatory.Unicode
     inserted = Mandatory.Unicode
     shift = Mandatory.Integer
-    startAA = Mandatory.Unicode
-    endAA = Mandatory.Unicode
+    start_aa = Mandatory.Unicode
+    end_aa = Mandatory.Unicode
     term = Mandatory.Integer
     hgvs = Mandatory.Unicode
-    hgvsLength = Mandatory.Integer
+    hgvs_length = Mandatory.Integer
 #RawVar
 
 