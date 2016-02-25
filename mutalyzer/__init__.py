d64398bfdb082c9617112101e96d60542903ddff
Martijn Vermaat <martijn@vermaat.name>
2016-02-25 14:25:15 +0100

    Open development for 2.0.17

--- mutalyzer/__init__.py
+++ mutalyzer/__init__.py
@@ -21,7 +21,7 @@ from __future__ import unicode_literals
 # [1] http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 # [2] http://semver.org/
 
-__version_info__ = ('2', '0', '16')
+__version_info__ = ('2', '0', '17', 'dev')
 __date__ = '25 Feb 2016'
 
 
