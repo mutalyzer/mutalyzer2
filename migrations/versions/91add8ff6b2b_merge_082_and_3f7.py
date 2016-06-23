a087154a347a6e9947c347c7db343e532341c6ed
Martijn Vermaat <martijn@vermaat.name>
2016-06-23 14:49:21 +0200

    Merge Alembic migrations

---
+++ migrations/versions/91add8ff6b2b_merge_082_and_3f7.py
@@ -0,0 +1,24 @@
+"""Merge 082 and 3f7
+
+Revision ID: 91add8ff6b2b
+Revises: (u'3f73b289d4b2', u'08220629322a')
+Create Date: 2016-06-23 14:47:42.807650
+
+"""
+
+from __future__ import unicode_literals
+
+# revision identifiers, used by Alembic.
+revision = '91add8ff6b2b'
+down_revision = (u'3f73b289d4b2', u'08220629322a')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
